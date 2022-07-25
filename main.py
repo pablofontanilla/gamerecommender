1850f21b87c0c62f1a1cc07ec82108d2322cac7a
Pablo Fontanilla <pfontanilla@medida.com>
2022-07-25 16:17:26 +0200

    Add library origin

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import json
 from time import sleep
 
+#pip install py-bgg
 from libbgg.apiv1 import BGG
 # You can also use version 2 of the api:
 from libbgg.apiv2 import BGG as BGG2
@@ -40,7 +41,7 @@ if __name__ == '__main__':
     owned_boardgames = bgg_client2.get_collection(username="chichaslocas", own=1, excludesubtype="boardgameexpansion")
 
     for game in owned_boardgames['items']['item']:
-        sleep(0.5)
+        sleep(2)
         game_name = game.name.TEXT
         full_game = bgg_client2.boardgame(game.objectid)['items']['item']
         recommended_player_count_poll = full_game.poll[0]
